16b51ec6f94e0995e884b0dfb863981459348aef
dohung <dohuyhung12@gmail.com>
2021-12-16 15:46:57 +0700

    add btn toggle read status

--- lib.js
+++ lib.js
@@ -26,39 +26,71 @@ function displayLibrary() {
 function displayBook(bookObj) {
   function createBookDetail(category) {
     let item = document.createElement("p");
+
     item.classList.add(`${category}`);
     item.innerText =
       category[0].toUpperCase() + category.slice(1) + ": " + bookObj[category];
-    book.appendChild(item);
+    bookElement.appendChild(item);
   }
 
-  let book = document.createElement("div");
-  book.classList.add("book");
-  book.setAttribute("data-id", myLibrary.indexOf(bookObj));
+  function createReadDetail() {
+    let item = document.createElement("p");
+    item.classList.add("read");
+    if (bookObj["read"]) {
+      item.innerText = "Read: " + "read";
+    } else {
+      item.innerText = "Read: " + "unread";
+    }
 
-  for (let i of ["title", "author", "pages", "read"]) {
+    bookElement.appendChild(item);
+  }
+
+  function addReadToggleBtn(bookElement) {
+    let readToggleBtn = document.createElement("button");
+    readToggleBtn.setAttribute("class", "btn read-toggle-btn");
+
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-book-open");
+
+    readToggleBtn.appendChild(icon);
+
+    readToggleBtn.addEventListener("click", () => {
+      bookObj.read = !bookObj.read;
+      bookElement.parentElement.removeChild(bookElement);
+      displayBook(bookObj);
+    });
+
+    bookElement.appendChild(readToggleBtn);
+  }
+  function addDelBtn(bookElement) {
+    let delBtn = document.createElement("button");
+    delBtn.setAttribute("class", "btn del-btn");
+
+    let icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-trash");
+
+    delBtn.appendChild(icon);
+
+    delBtn.addEventListener("click", () => {
+      const index = bookElement.dataset.id;
+
+      myLibrary.splice(index, 1);
+    });
+
+    bookElement.appendChild(delBtn);
+  }
+  let bookElement = document.createElement("div");
+  bookElement.classList.add("book");
+  bookElement.setAttribute("data-id", myLibrary.indexOf(bookObj));
+
+  for (let i of ["title", "author", "pages"]) {
     createBookDetail(i);
   }
 
-  addDelBtn(book);
+  createReadDetail();
+  addReadToggleBtn(bookElement);
 
-  bookContainer.appendChild(book);
-}
+  addDelBtn(bookElement);
 
-function addDelBtn(item) {
-  let delBtn = document.createElement("button");
-  delBtn.setAttribute("class", "btn del-btn");
-
-  let icon = document.createElement("i");
-  icon.setAttribute("class", "fas fa-trash");
-
-  delBtn.appendChild(icon);
-
-  delBtn.addEventListener("click", () => {
-    const index = item.dataset.id;
-    item.parentElement.removeChild(item);
-    myLibrary.splice(index, 1);
-  });
-
-  item.appendChild(delBtn);
+  bookContainer.appendChild(bookElement);
 }
